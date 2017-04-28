e2d99cf2e0fd0207a0ec7c51973821aa7c91c5d7
zhenggangchen <458409991@qq.com>
2017-04-28 15:39:33 +0800

    实现jQuery插件框架

---
+++ pageswitch.js
@@ -0,0 +1,79 @@
+/**
+ * Created by czg on 2017/4/28.
+ */
+(function ($) {
+    var privateFun= function () {
+        //
+    }
+    var PageSwitch = (function () {
+        function PageSwitch(element, options) {
+            this.settings = $.extend(true, $.fn.PageSwitch.default, options || {});
+            this.element = element;
+            this.init();
+        }
+
+        PageSwitch.prototype = {
+            init: function () {
+
+            }
+        }
+        return PageSwitch;
+    })();
+    $.fn.PageSwitch = function (options) {
+        return this.each(function () {
+            var me = $(this), instance = me.data("PageSwitch");
+            if (!instance) {
+                instance = new PageSwitch(me, options);
+                me.data("PageSwitch", instance);
+            }
+
+            if ($.type(options) === "string")return instance[options]();
+            $("div").PageSwitch("init");
+
+        });
+
+    }
+    $.fn.PageSwitch.default = {
+        selecters: {
+            sections: ".sections",
+            section: ".section",
+            page: ".pages",
+            active: ".active"
+        },
+        index: 0,
+        easing: "ease",
+        duration: 500,
+        loop: false,
+        pagination: true,
+        keyboard: true,
+        direction: "vertical",
+        callback: ""
+    }
+    $(function () {
+        $("[data-PageSwitch]").PageSwitch();
+    })
+    
+    
+    
+    
+    
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
